73d4b58fcbeb642ed848c452b3ba9ede4eea5e0a
Akhadjon Bokijanov <cw1.fit.00007510@gmail.com>
2021-02-06 16:43:51 +0500

    postgre column name issue

--- app/Http/Controllers/CabinetController.php
+++ app/Http/Controllers/CabinetController.php
@@ -53,35 +53,35 @@ class CabinetController extends Controller
                 $user = $this->user();
                 $q->where('sellerTin', $user["tin"])
                     ->orWhere([["buyerTin", $user["tin"]]], ["state", "!=", 1] );
-            })->select(['facturaNo as docNo', "sellerName", "sellerTin", "buyerTin", "buyerName", "created_at", "contractNo", DB::raw("'factura' as docType"), "status"]);
+            })->select(['facturaNo as docNo', "sellerName", "sellerTin", "buyerTin", "buyerName", "created_at", "contractNo", DB::raw("'factura' as \"docType\""), "status"]);
 
         $acts = DB::table('acts')
             ->where(function ($q){
                 $user = $this->user();
                 $q->where('sellerTin', $user["tin"])
                     ->orWhere([["buyerTin", $user["tin"]]], ["state", "!=", 1] );
-            })->select(['actNo as docNo', "sellerName", "sellerTin", "buyerTin", "buyerName", "created_at", "contractNo", DB::raw("'act' as docType"), "status"]);
+            })->select(['actNo as docNo', "sellerName", "sellerTin", "buyerTin", "buyerName", "created_at", "contractNo", DB::raw("'act' as \"docType\""), "status"]);
 
         $empowerments = DB::table('empowerments')
             ->where(function ($q){
                 $user = $this->user();
                 $q->where('sellerTin', $user["tin"])
                     ->orWhere([["buyerTin", $user["tin"]]], ["state", "!=", 1] );
-            })->select(['empowermentNo as docNo', "sellerName", "sellerTin", "buyerTin", "buyerName", "created_at", "contractNo", DB::raw("'empowerment' as docType"), "status"]);
+            })->select(['empowermentNo as docNo', "sellerName", "sellerTin", "buyerTin", "buyerName", "created_at", "contractNo", DB::raw("'empowerment' as \"docType\""), "status"]);
 
         $contracts = DB::table('contracts')
             ->where(function ($q){
                 $user = $this->user();
                 $q->where('sellerTin', $user["tin"])
                     ->orWhere([["buyerTin", $user["tin"]]], ["state", "!=", 1] );
-            })->select(['contractNo as docNo', "sellerName", "sellerTin", "buyerTin", "buyerName", "created_at", "contractNo",DB::raw("'contract' as docType"), "status"]);
+            })->select(['contractNo as docNo', "sellerName", "sellerTin", "buyerTin", "buyerName", "created_at", "contractNo",DB::raw("'contract' as \"docType\""), "status"]);
 
         $ttys = DB::table('carrier_way_bills')
             ->where(function ($q){
                 $user = $this->user();
                 $q->where('sellerTin', $user["tin"])
                     ->orWhere([["buyerTin", $user["tin"]]], ["state", "!=", 1] );
-            })->select(['contractNo as docNo', "sellerName", "sellerTin", "buyerTin", "buyerName", "created_at", "contractNo",DB::raw("'tty' as docType"), "status"]);
+            })->select(['contractNo as docNo', "sellerName", "sellerTin", "buyerTin", "buyerName", "created_at", "contractNo",DB::raw("'tty' as \"docType\""), "status"]);
 
         $all = $facturas->union($ttys)
             ->union($acts)
